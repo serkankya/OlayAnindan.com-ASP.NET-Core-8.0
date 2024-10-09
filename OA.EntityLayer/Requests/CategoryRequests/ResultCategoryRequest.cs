24599b0ca1fdf7d3870399cd9d6c4d6ddd8ed559
Serkan Kaya <serkankaya0721@gmail.com>
2024-10-09 15:05:39 +0300

    Category controller and management page created.

---
+++ OA.EntityLayer/Requests/CategoryRequests/ResultCategoryRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA.EntityLayer.Requests.CategoryRequests
+{
+	public class ResultCategoryRequest
+	{
+        public int CategoryId { get; set; }
+        public string? CategoryName { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ OA.UserInterface/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using OA.EntityLayer.Requests.CategoryRequests;
+using OA.UserInterface.Models;
+
+namespace OA.UserInterface.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class CategoryController : Controller
+	{
+		readonly IHttpClientFactory _httpClientFactory;
+		readonly ApiSettings _apiSettings;
+
+		public CategoryController(IHttpClientFactory httpClientFactory, IOptions<ApiSettings> apiSettings)
+		{
+			_httpClientFactory = httpClientFactory;
+			_apiSettings = apiSettings.Value;
+		}
+
+		public async Task<IActionResult> Manage()
+		{
+			var client = _httpClientFactory.CreateClient();
+			client.BaseAddress = new Uri(_apiSettings.BaseHostUrl!);
+			var response = await client.GetAsync("Category/GetAll");
+
+			if (response.IsSuccessStatusCode)
+			{
+				var jsonData = await response.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultCategoryRequest>>(jsonData);
+
+				return View(values);
+			}
+
+			return View();
+		}
+
+		public async Task<IActionResult> RemoveCategory(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			client.BaseAddress = new Uri(_apiSettings.BaseHostUrl!);
+			var content = new StringContent(string.Empty);
+
+			var response = await client.PutAsync("Category/Remove/" + id, content);
+
+			if (response.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Manage", "Category");
+			}
+
+			return View();
+		}
+	}
+}
