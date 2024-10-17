47438ca48c98e3f6c33df34a514d9e2a85cc41c0
Serkan Kaya <serkankaya0721@gmail.com>
2024-10-17 16:25:18 +0300

    Component partials for main news and featured news are completed.

--- OA.UserInterface/Areas/User/Controllers/HomeController.cs
+++ OA.UserInterface/Areas/User/Controllers/HomeController.cs
@@ -1,13 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using OA.EntityLayer.Requests.ArticleRequests;
+using OA.UserInterface.Models;
 
 namespace OA.UserInterface.Areas.User.Controllers
 {
-    [Area("User")]
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+	[Area("User")]
+	public class HomeController : Controller
+	{
+		readonly IHttpClientFactory _httpClientFactory;
+		readonly ApiSettings _apiSettings;
+
+		public HomeController(IHttpClientFactory httpClientFactory, IOptions<ApiSettings> apiSettings)
+		{
+			_httpClientFactory = httpClientFactory;
+			_apiSettings = apiSettings.Value;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
 }
---
+++ OA.UserInterface/Areas/User/ViewComponents/_HomeGetFeaturedNewsComponentPartial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using OA.EntityLayer.Requests.ArticleRequests;
+using OA.UserInterface.Models;
+using System.Net.Http;
+
+namespace OA.UserInterface.Areas.User.ViewComponents
+{
+    public class _HomeGetFeaturedNewsComponentPartial : ViewComponent
+    {
+        readonly IHttpClientFactory _httpClientFactory;
+        readonly ApiSettings _apiSettings;
+
+        public _HomeGetFeaturedNewsComponentPartial(IHttpClientFactory httpClientFactory, IOptions<ApiSettings> apiSettings)
+        {
+            _httpClientFactory = httpClientFactory;
+            _apiSettings = apiSettings.Value;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri(_apiSettings.BaseHostUrl!);
+            var response = await client.GetAsync("Article/GetFeaturedNews");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultArticleRequest>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+    }
+}
