8a519e8f89755ce83e0c7aedaa7779dd46950a92
Serkan Kaya <serkankaya0721@gmail.com>
2024-10-21 21:33:45 +0300

    Comment posting section completed successfully.

--- OA.UserInterface/Areas/User/Controllers/NewsController.cs
+++ OA.UserInterface/Areas/User/Controllers/NewsController.cs
@@ -1,31 +1,62 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using OA.EntityLayer.Requests.CommentRequests;
+using OA.UserInterface.Models;
+using System.Text;
 
 namespace OA.UserInterface.Areas.User.Controllers
 {
-    [Area("User")]
-    public class NewsController : Controller
-    {
-        public IActionResult Index()
-        {
-            ViewBag.CategoryId = TempData["CategoryId"];
-            ViewBag.DateOpt = TempData["DateOpt"];
-            
-            return View();
-        }
+	[Area("User")]
+	public class NewsController : Controller
+	{
+		readonly IHttpClientFactory _httpClientFactory;
+		readonly ApiSettings _apiSettings;
 
-        [HttpGet]
-        public IActionResult FilterNews(int categoryId, bool dateOpt)
-        {
-            TempData["CategoryId"] = categoryId;
-            TempData["DateOpt"] = dateOpt;
+		public NewsController(IHttpClientFactory httpClientFactory, IOptions<ApiSettings> apiSettings)
+		{
+			_httpClientFactory = httpClientFactory;
+			_apiSettings = apiSettings.Value;
+		}
 
-            return RedirectToAction("Index","News", new {area="User"});
-        }
+		public IActionResult Index()
+		{
+			ViewBag.CategoryId = TempData["CategoryId"];
+			ViewBag.DateOpt = TempData["DateOpt"];
+
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult FilterNews(int categoryId, bool dateOpt)
+		{
+			TempData["CategoryId"] = categoryId;
+			TempData["DateOpt"] = dateOpt;
+
+			return RedirectToAction("Index", "News", new { area = "User" });
+		}
 
 		[Route("/User/News/SingleNews/{articleId}")]
 		public IActionResult SingleNews(int articleId)
 		{
 			ViewBag.SelectedArticledId = articleId;
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> LeaveComment(InsertCommentRequest insertCommentRequest)
+		{
+			var client = _httpClientFactory.CreateClient();
+			client.BaseAddress = new Uri(_apiSettings.BaseHostUrl!);
+			var jsonData = JsonConvert.SerializeObject(insertCommentRequest);
+			StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			var response = await client.PostAsync("Comment/Insert", stringContent);
+
+			if (response.IsSuccessStatusCode)
+			{
+				return RedirectToAction("SingleNews", "News", new { area = "User", articleId = insertCommentRequest.ArticleId });
+			}
+
 			return View();
 		}
 	}
