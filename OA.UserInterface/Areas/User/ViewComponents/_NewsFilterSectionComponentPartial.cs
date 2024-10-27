b0fb3df9c5322a3a4b4c150b27d874a44b0f4f9d
Serkan Kaya <serkankaya0721@gmail.com>
2024-10-27 20:26:25 +0300

    Categories listed in select menu. Contact page created.

--- OA.UserInterface/Areas/User/ViewComponents/_NewsFilterSectionComponentPartial.cs
+++ OA.UserInterface/Areas/User/ViewComponents/_NewsFilterSectionComponentPartial.cs
@@ -1,12 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using OA.EntityLayer.Requests.CategoryRequests;
+using OA.UserInterface.Models;
 
 namespace OA.UserInterface.Areas.User.ViewComponents
 {
-    public class _NewsFilterSectionComponentPartial : ViewComponent
-    {
-        public IViewComponentResult Invoke()
-        {
-            return View();
-        }
-    }
+	public class _NewsFilterSectionComponentPartial : ViewComponent
+	{
+		readonly IHttpClientFactory _httpClientFactory;
+		readonly ApiSettings _apiSettings;
+
+		public _NewsFilterSectionComponentPartial(IHttpClientFactory httpClientFactory, IOptions<ApiSettings> apiSettings)
+		{
+			_httpClientFactory = httpClientFactory;
+			_apiSettings = apiSettings.Value;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpClientFactory.CreateClient();
+			client.BaseAddress = new Uri(_apiSettings.BaseHostUrl!);
+			var response = await client.GetAsync("Category/GetActives");
+
+			if (response.IsSuccessStatusCode)
+			{
+				var jsonData = await response.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultCategoryRequest>>(jsonData);
+
+				return View(values);
+			}
+
+			return View();
+		}
+	}
 }
