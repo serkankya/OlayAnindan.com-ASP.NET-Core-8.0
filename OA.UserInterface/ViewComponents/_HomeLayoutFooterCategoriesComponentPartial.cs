bc621e268e57619b837bd080e7255270af8302ef
Serkan Kaya <serkankaya0721@gmail.com>
2024-11-01 14:15:42 +0300

    Partial views modified for home layout.

---
+++ OA.UserInterface/ViewComponents/_HomeLayoutFooterCategoriesComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OA.UserInterface.ViewComponents
+{
+	public class _HomeLayoutFooterCategoriesComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
---
+++ OA.UserInterface/ViewComponents/_HomeLayoutJavascriptComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OA.UserInterface.ViewComponents
+{
+	public class _HomeLayoutJavascriptComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
